27c4b3491cc8333c105ce18cdfa5feb30eafc0bb
Noam <noam_harris@hotmail.com>
2022-11-24 13:18:10 +0200

    height fixed

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,10 @@ function App() {
   const {dark} = DarkTheme();
   const { todoList } =TodoList()
   
-  const screenHeightStyle = {
-  height:todoList.length > 4?"100%":"100vh"
-}
+
 console.log(todoList.length)
   return (
-    <div className={dark?"applight":"App"}  style={screenHeightStyle}>  
+    <div className={dark?"applight":"App"}  >  
       <Header/>
       <Todo/>
       </div>
